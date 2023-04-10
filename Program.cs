92d18d43e909fa8daa6ce80a9aecb9769c4b7a4c
Paulo Henrique Gonçalves de Oliveira <paulo.oliveira.phgo@gmail.com>
2023-04-10 10:30:25 -0300

    exercicio executado

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Faça um programa que receba um número inteiro e mostre a sua tabuada.
+
+//Entrada
+Console.WriteLine($"Insira um número inteiro: ");
+int numero = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Tabuada de {numero}");
+
+//Processamento
+for (int i = 0; i <=10; i++)
+{
+Console.WriteLine($"{numero} X {i} = {numero*i}");
+}
+//Saida
+
